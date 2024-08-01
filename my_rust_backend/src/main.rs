6eebc50aaedf811f17af9d45108a28a689c3c6fa
Justin <roblujm@gmail.com>
2024-07-31 21:15:44 -0400

    merged the two main.rs files

--- my_rust_backend/src/main.rs
+++ my_rust_backend/src/main.rs
@@ -1,10 +1,8 @@
-// Add modules
 mod test;
 mod csv_reader;
-
 use std::collections::BTreeMap;
 use axum::{
-    extract::{Json, Extension},
+    extract::{Json, Extension, Query},
     response::IntoResponse,
     routing::{get, post},
     Router,
@@ -23,16 +21,37 @@ use csv_reader::reading_csv;
 use test::Novel;
 use crate::test::{FindNovel, Graph};
 
-type SharedState = Arc<Mutex<Vec<Novel>>>;
+type SharedState = Arc<Mutex<AppState>>;
 
-#[derive(Deserialize)]
+
+#[derive(Debug, Deserialize, Clone)]
+
 struct InputData {
     input: String,
+    input2: String,
+    checked: bool,
 }
 
-async fn handle_input(Json(data): Json<InputData>) -> impl IntoResponse {
+struct AppState {
+    novels: Vec<Novel>,
+    result: Vec<String>,
+}
+
+
+
+async fn handle_input(Json(data): Json<InputData>, Extension(state): Extension<SharedState>) -> impl IntoResponse {
     println!("Received input: {}", data.input);
+    println!("Received input: {}", data.input2);
+    println!("Received input: {}", data.checked);
+
+    let mut state = state.lock().unwrap();
+    state.result.clear();
+    state.result.push(data.input.clone());
+    state.result.push(data.input2.clone());
+    state.result.push(data.checked.to_string());
+
     "Input received"
+
 }
 
 #[tokio::main]
@@ -40,14 +59,24 @@ async fn main() {
     // Get a vector of ONLY SFW novels
     let novels = reading_csv();
 
+
     let mut num_data_points = 0;
     for novel in &novels {
         num_data_points += novel.tag_cont.len() + novel.seiyuu.len();
     }
     println!("Number of data points in sfw novels ONLY: {}", num_data_points);
 
+    /* let test_novel1: usize = find_novel(&novels, 11).await; // Fate/Stay Night
+    let test_novel2: usize = find_novel(&novels, 50).await; // Fate/Stay Night Ataraxia - Direct Sequel
+    novels[test_novel1].print_novel();
+    novels[test_novel2].print_novel();
+    println!("{}", novels[test_novel1].comparing(&novels[test_novel2]));*/
     let novel_graph = get_weights(&novels).await;
 
+    /* let test_novel3: usize = find_novel(&sfw_novels, 971).await;
+    if test_novel3 < sfw_novels.len() {
+        sfw_novels[test_novel3].print_novel();
+    } */
     let dijkstra_path1 = novel_graph.dijkstra(&(19119u16), &(14908u16), novels.clone());
     // Fate/EXTELLA being compared to Code:Realize -> NO PATH FOUND
 
@@ -85,8 +114,7 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(Level::INFO)
         .init();
-
-    // CORS configuration to allow requests from any origin
+// CORS configuration to allow requests from any origin
     let cors = CorsLayer::new()
         .allow_origin(AllowOrigin::any())
         .allow_methods(AllowMethods::any())
@@ -95,32 +123,23 @@ async fn main() {
     // TODO: Tim: I changed the attributes of the Novel struct
     //         try using novels : Vec<Novel> instead.
     // Shared state with initial data
-    let shared_state = Arc::new(Mutex::new(/*vec![*/novels
-                                           // Novel {
-                                           //     title: String::from("Person X"),
-                                           //     v_id: 36,
-                                           //     favourite_food: Some(String::from("Pizza")),
-                                           // },
-                                           // Novel {
-                                           //     title: String::from("Person B"),
-                                           //     v_id: 5,
-                                           //     favourite_food: Some(String::from("Broccoli")),
-                                           // },
-                                           // Novel {
-                                           //     title: String::from("Person C"),
-                                           //     v_id: 100,
-                                           //     favourite_food: None,
-                                           // }
-                                           /*]*/));
+    let shared_state = Arc::new(Mutex::new(AppState{novels,result: vec![],}));
+
+
+    clearresult(&shared_state);
+    addresult(&shared_state, "test".to_string());
+
+                                          
 
     // Define routes and apply middleware
     let app = Router::new()
         .route("/", get(root))
         .route("/people", get(get_people))
         .route("/input", post(handle_input))
+        .route("/result", get(get_result))
         .layer(cors)
         .layer(Extension(shared_state));
-
+    
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
 
@@ -129,18 +148,46 @@ async fn main() {
         .await
         .unwrap();
 }
-
 async fn root() -> &'static str {
     "Hello World"
 }
+fn resultadd(state: &mut AppState, entry: String) {
+    state.result.push(entry);
+}
+
+fn resultclear(state: &mut AppState) {
+    state.result.clear();
+}
+
+fn clearresult(shared_state: &SharedState) {
+    let mut state = shared_state.lock().unwrap();
+    resultclear(&mut state);
+}
+
+fn addresult(shared_state: &SharedState, entry: String) {
+    let mut state = shared_state.lock().unwrap();
+    resultadd(&mut state, entry);
+}
+
+
+
+async fn get_result( 
+    Extension(state): Extension<SharedState>,) -> impl IntoResponse {
+    let mut state = state.lock().unwrap();
+
+    // resultclear(&mut state);
+    // resultadd(&mut state, String::from("test"));
+
+
+    Json(state.result.clone())
+}
 
 async fn get_people(
-    Extension(state): Extension<SharedState>,
-) -> impl IntoResponse {
+    Extension(state): Extension<SharedState>,) -> impl IntoResponse {
     match state.lock() {
         Ok(novels) => {
             info!("Successfully fetched people data");
-            Json(novels.clone()).into_response()
+            Json(novels.novels.clone()).into_response()
         },
         Err(e) => {
             error!("Failed to acquire lock: {:?}", e);
@@ -150,7 +197,8 @@ async fn get_people(
 }
 
 // Returns a TreeMap of <v_id, Vec<v_id, weight>>
-/*async fn get_weights(novels: &Vec<Novel>) -> BTreeMap<u16, Vec<(u16, u8)>> { // TODO: THIS WILL RETURN SOMETHING
+ // TODO: THIS WILL RETURN SOMETHING
+async fn get_weights(novels: &Vec<Novel>) -> BTreeMap<u16, Vec<(u16, u8)>> { // TODO: THIS WILL RETURN SOMETHING
     let mut graph= BTreeMap::new();
     for from in 0..novels.len() {
         println!("{}, {}", novels[from].title, novels[from].v_id);
@@ -174,25 +222,26 @@ async fn get_people(
             }
             // End if
         }
+        
     }
-    graph
-}*/
+    graph}
 
-async fn get_weights(novels: &Vec<Novel>) -> BTreeMap<u16, Vec<(u16, u8)>> {     // TODO: THIS WILL RETURN SOMETHING
-    let mut graph: BTreeMap<u16, Vec<(u16,u8)>> = BTreeMap::new();
-    for from in 0..novels.len(){ // Comparing 'from' novel to every other novel after it.
-        // println!("{}, {}", novels[from].title, novels[from].v_id);
-        let mut weights: Vec<(u16, u8)> = vec![];
-        for to in 0..novels.len(){
-            if to != from {
-                let similarity: u8 = novels[from].comparing(&novels[to]);
 
-                if similarity < 112{
-                    weights.push((novels[to].v_id, similarity));
-                }
-            }
-        }
-        graph.insert(novels[from].v_id, weights.clone());
-    }
-    graph
-}
+// async fn get_weights(novels: &Vec<Novel>) -> BTreeMap<u16, Vec<(u16, u8)>> {     // TODO: THIS WILL RETURN SOMETHING
+//     let mut graph: BTreeMap<u16, Vec<(u16,u8)>> = BTreeMap::new();
+//     for from in 0..novels.len(){ // Comparing 'from' novel to every other novel after it.
+//         // println!("{}, {}", novels[from].title, novels[from].v_id);
+//         let mut weights: Vec<(u16, u8)> = vec![];
+//         for to in 0..novels.len(){
+//             if to != from {
+//                 let similarity: u8 = novels[from].comparing(&novels[to]);
+
+//                 if similarity < 112{
+//                     weights.push((novels[to].v_id, similarity));
+//                 }
+//             }
+//         }
+//         graph.insert(novels[from].v_id, weights.clone());
+//     }
+//     graph
+// }
